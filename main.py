a86fa0c9ef344b95740ced2717c91a3e1e379ce8
Julian Alejandro Hernandez Bohorquez <j.hernandez.bo@gmail.com>
2020-11-25 13:54:40 -0500

    Add steps V1.0

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template,session
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__) 
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = '123456aaa'
 
 todos = ['A','B','C']
 
@@ -22,21 +23,32 @@ def server_error(error):
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
+    response = make_response(redirect('/welcome'))
+    session['user_ip'] = user_ip
+    #response.set_cookie('user_ip',user_ip)
     return response
 
-@app.route('/hello')
-def hello():
+
+@app.route('/welcome')
+def welcome():
 
 
-    user_ip = request.cookies.get('user_ip')
+    #user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     if user_ip:   
         context = {
             'user_ip':user_ip,
             'todos':todos
         }
-        return render_template('hello.html',**context)#** permite expandir el diccionario
+        return render_template('welcome.html',**context)#** permite expandir el diccionario
     else:
         response = make_response(redirect('/'))
         return response
+
+
+@app.route('/step/<int:st>')
+def steps(st):
+    context = {
+        'st':st,
+    }
+    return render_template('steps/step_'+ str(st) +'.html',**context)
